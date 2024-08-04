100aa04361f6364bbea3a900eac58dd3c35aebb3
javohirtohirovich <javoergashev30@gmail.com>
2024-08-04 17:25:55 +0500

    Add project files.

---
+++ RoboContestProblemSolutions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RoboContestProblemSolutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
